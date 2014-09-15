84e8580d25838eb4c562bfd1947c8972d8752af1
angel.munher <angel.munher@gmail.com>
2014-09-16 00:48:29 +0200

    Integración de socket.io (express socket.io) y librería adapter.js para webrtc

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('./bbdd')
 
-var express = require('express'),
+var express_io = require('express.io'),
   https = require('https'),
   fs = require('fs'),
   path = require('path'),
@@ -16,24 +16,25 @@ var express = require('express'),
     rejectUnauthorized: false
   },
 
-  app = express();
+  app = express_io()
+  app.https(sslOptions).io();
 
 /******************************/
 /* Configuración del servidor */
 /******************************/
 app.configure(function () {
   app.set('port', process.env.PORT || config.port);
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use( express.cookieParser( config.cookieSecret ) );
-  app.use( express.cookieSession( config.sessionSecret ) );
+  app.use(express_io.logger('dev'));
+  app.use(express_io.bodyParser());
+  app.use(express_io.cookieParser( config.cookieSecret ) );
+  app.use(express_io.cookieSession( config.sessionSecret ) );
 
   app.set('view engine', 'html');
   app.set('views', __dirname + '/views' );
   app.engine('html', require('hbs').__express);
 
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.csrf());
+  app.use(express_io.static(path.join(__dirname, 'public')));
+  app.use(express_io.csrf());
   app.enable('trust proxy');
 });
 
@@ -42,7 +43,6 @@ app.configure(function () {
 /*******************************/
 /* Peticiones de la aplicación */
 /*******************************/
-
 app.get("/", function(req, res){
   res.render('index.html', { csrfToken: req.csrfToken() });
 });
@@ -53,6 +53,8 @@ app.post('/api/auth', auth.signIn);
 app.post('/api/auth/signup', auth.signUp);
 
 app.post('/api/auth/logout', auth.logOut);
+
+app.post('/api/auth/close', auth.closeWindow);
 
 app.get('/api/home', users.getUserInfo);
 
@@ -70,11 +72,49 @@ app.get('/mock/home', function (req, res) {
   });
 });
 
-/**********************************/
-/* Arranque de los servidores     */
-/**********************************/
-app.listen(app.get('port'));
+/*************************************/
+/* Peticiones de la videoconferencia */
+/*************************************/
+app.io.sockets.on('connection', function (socket){
 
-var secureServer = https.createServer(sslOptions,app).listen(config.sslPort, function(){
-  console.log("Servidor https escuchando en el puerto: " + config.sslPort);
+  function log(){
+    var array = [">>> "];
+    for (var i = 0; i < arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+      socket.emit('log', array);
+  }
+
+  socket.on('message', function (message) {
+    log('Got message: ', message);
+    socket.broadcast.emit('message', message); // should be room only
+  });
+
+  socket.on('create or join', function (room) {
+    var numClients = app.io.sockets.clients(room).length;
+
+    log('Room ' + room + ' has ' + numClients + ' client(s)');
+    log('Request to create or join room', room);
+
+    if (numClients == 0){
+      socket.join(room);
+      socket.emit('created', room);
+    } else if (numClients == 1) {
+      app.io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room);
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+    socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+    socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+  });
+
 });
+
+/*******************************/
+/* Arranque del servidores     */
+/*******************************/
+
+app.listen(app.get('port'));
