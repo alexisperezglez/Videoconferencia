83c4943e590ef04c2aa8df3e34ff75e4cb85f66e
angel.munher <angel.munher@gmail.com>
2014-09-16 00:49:05 +0200

    Información de sesiones en base de datos

--- api/auth.js
+++ api/auth.js
@@ -10,7 +10,11 @@ exports.getAuth = function (req, res) {
       next(err);
     }
     if (!_.isEmpty(user)) {
+      User.findOneAndUpdate({ _id: user._id }, { state : 0 }, { upsert: false }, function (err, user) {
+        console.log(user);
+      });
       res.json({ user: _.omit(user, ['hash', '__v']), logado: true, message: 'Usuario logado'});
+}
     } else {
       res.json({ user: {}, message: 'Usuario nuevo', logado: false });
     }
@@ -21,7 +25,9 @@ exports.signIn = function (req, res) {
   User.findOne({ username: req.body.username }).lean().exec(function (err, user) {
     if (user) {
       if( bcrypt.compareSync( req.body.password, user.hash )){
-        User.findOneAndUpdate({ _id: user._id }, { state : 0 });
+        User.findOneAndUpdate({ _id: user._id }, { state : 0 }, { upsert: false }, function (err, user) {
+          console.log(user);
+        });
         res.cookie('user_id', user._id, { signed: true, maxAge: config.cookieMaxAge });
         res.cookie('auth_token', user.hash, { signed: true, maxAge: config.cookieMaxAge });
         res.json({ user: _.omit(user, ['hash', '__v']), logado: true, message: 'Usuario logado con éxito', status: 'OK' });
@@ -53,8 +59,16 @@ exports.signUp = function (req, res) {
 }
 
 exports.logOut = function (req, res) {
-  User.findOneAndUpdate({ _id: req.signedCookies.user_id }, { state : 1 });
+  User.findOneAndUpdate({ _id: req.signedCookies.user_id }, { state : 1 }, { upsert: false }, function (err, user) {
+    console.log(user);
+  });
   res.clearCookie('user_id');
   res.clearCookie('auth_token');
   res.redirect('/');
+}
+
+exports.closeWindow = function (req, res) {
+  User.findOneAndUpdate({ _id: req.signedCookies.user_id }, { state : 1 }, { upsert: false }, function (err, user) {
+    console.log(user);
+  });
 }
